4b5b343f1e1231c3eb9c9612b5b8ee9ea67d4f7a
Famdil310 <farouqeldian@gmail.com>
2021-03-21 12:49:20 +0700

    1.2 Perulangan/Switch

---
+++ bin/Dart_if.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+// ignore: always_declare_return_types
+main(List<String> arguments) {
+  int number = int.tryParse(stdin.readLineSync());
+
+  if (number > 0) {
+    print("nol");
+  } else if (number = 1) {
+    print("satu");
+  } else if (number = 2) {
+    print("dua");
+  } else {
+    print("bilangan lain");
+  }
+}

8224e9a735359d0a6b700ad8ed1401f1b8ebf850
FamFadil310 <79836982+Famdil310@users.noreply.github.com>
2021-03-28 18:55:09 +0700

    Create TunjanganAnak.dart
    SE1 DasarDart - 3_Tunjangan_Anak

---
+++ TunjanganAnak.dart
@@ -0,0 +1,14 @@
+Input(iGajiPokok)
+Input(iJmlAnak)
+
+if(iJmlAnak <= 3 && iJmlAnak > 0) Then
+  iTunjangan <- iJmlAnak * 0.1 * iGajiPokok
+Else if (iJmlAnak > 3)
+  iTunjangan <- 3 * 0.1 iGajiPokok
+Else
+  iTunjangan <- 0
+End if
+  
+Output (iTunjangan)
+  
+End

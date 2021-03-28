7373fd3017f6ea0821addc5b46cfbf6a982cf358
FamFadil310 <79836982+Famdil310@users.noreply.github.com>
2021-03-28 19:53:53 +0700

    Update SE1-Soal3-TunAnak.dart

--- bin/SE1-Soal3-TunAnak.dart
+++ bin/SE1-Soal3-TunAnak.dart
@@ -2,11 +2,11 @@ Input(iGajiPokok)
 Input(iJmlAnak)
 
 if(iJmlAnak <= 3 && iJmlAnak > 0) Then
-iTunjangan <- iJmlAnak * 0.1 * iGajiPokok
+  iTunjangan <- iJmlAnak * 0.1 * iGajiPokok
 Else if (iJmlAnak > 3)
-iTunjangan <- 3 * 0.1 iGajiPokok
+  iTunjangan <- 3 * 0.1 iGajiPokok
 Else
-iTunjangan <- 0
+  iTunjangan <- 0
 End if
 
 Output (iTunjangan)
